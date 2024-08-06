47c4129a318f4b645cc06832a63d60d437da618b
angelo <angeloraphael1@hotmail.com>
2024-08-06 16:57:36 -0300

    feat: hex decoder architecture

--- 3-1/hex-to-7seg-decoder.vhd
+++ 3-1/hex-to-7seg-decoder.vhd
@@ -4,24 +4,50 @@ use IEEE.std_logic_1164.all;
 
 ENTITY hex_to_seg_decoder IS
 PORT (
-	x0: IN STD_LOGIC;
-	x1: IN STD_LOGIC;
-	x2: IN STD_LOGIC;
-	x3: IN STD_LOGIC;
-
-	a: IN STD_LOGIC;
-	b: IN STD_LOGIC;
-	c: IN STD_LOGIC;
-	d: IN STD_LOGIC;
-	e: IN STD_LOGIC;
-	f: IN STD_LOGIC;
-	g: IN STD_LOGIC
-
-     );
+	entry: IN STD_LOGIC_VECTOR(0 to 3);
+	segs: OUT STD_LOGIC_VECTOR(0 to 6)
+	);
 END hex_to_seg_decoder;
 
 ARCHITECTURE impl OF hex_to_seg_decoder IS
 BEGIN
+	PROCESS (entry)
+	BEGIN
+		CASE entry IS
+		when "0000" =>
+			segs <= "1111110";
+		when "0001" =>
+			segs <= "0110000";
+		when "0010" =>
+			segs <= "1101101";
+		when "0011" =>
+			segs <= "1111001";
+		when "0100" =>
+			segs <= "0110011";
+		when "0101" =>
+			segs <= "1011011";
+		when "0110" =>
+			segs <= "1011111";
+		when "0111" =>
+			segs <= "1110000";
+		when "1000" =>
+			segs <= "1111111";
+		when "1001" =>
+			segs <= "1111011";
+		when "1010" =>
+			segs <= "";
+		when "1011" =>
+			segs <= "";
+		when "1100" =>
+			segs <= "";
+		when "1101" =>
+			segs <= "";
+		when "1110" =>
+			segs <= "";
+		when "1101" =>
+			segs <= "";
 
+	END CASE;
+END PROCESS;
 END impl;
 
